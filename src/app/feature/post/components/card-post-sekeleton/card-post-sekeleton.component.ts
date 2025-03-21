0944c422fe2d2a8b22c4ed719eef5083830df9fa
JoseTorresRyndem <jose.torres@ryndem.mx>
2025-03-20 23:57:51 -0600

    feat: Add CardPostSkeleton component with HTML, SCSS, and TypeScript files

---
+++ src/app/feature/post/components/card-post-sekeleton/card-post-sekeleton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-post-sekeleton',
+  templateUrl: './card-post-sekeleton.component.html',
+  styleUrls: ['./card-post-sekeleton.component.scss']
+})
+export class CardPostSekeletonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
